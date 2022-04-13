7f85b21b1b7565aaa5ae755cdbad9d63fc0d8180
Andrea Giardina <andrea@etiqa.it>
2022-04-13 22:44:02 +0200

    Task 1.1

--- minitorch/scalar.py
+++ minitorch/scalar.py
@@ -22,8 +22,14 @@ def central_difference(f, *vals, arg=0, epsilon=1e-6):
     Returns:
         float : An approximation of :math:`f'_i(x_0, \ldots, x_{n-1})`
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError('Need to implement for Task 1.1')
+
+    a = list(vals)
+    a[arg] += epsilon
+
+    b = list(vals)
+    b[arg] -= epsilon
+
+    return (f(*a) - f(*b))/(2*epsilon)
 
 
 # ## Task 1.2 and 1.4
